555a75dc79480025b16feb1fc1d20fe88174d03b
cjrhansen2 <cjrhansen2@gmail.com>
2021-02-23 16:20:09 -0800

    server fixed

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const mongoose = require("mongoose");
 const path = require("path");
 const bodyparser = require("body-parser");
 
-//pull in the routes for the server from the route directory
-const route_view = require("./route/view");
-const route_api = require("./route/api");
-
 //make the express app and create the port
 const app = express();
 const PORT = process.env.PORT || 7000;
@@ -18,15 +14,16 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-//use the routes created
-app.use(route_view);
-app.use(route_api);
 
 //make the mongoose connection to connect to mongodb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+//pull in the routes for the server from the route directory
+require("./route/view")(app);
+require("./route/api")(app);
 
 //then have the server start to listen
 app.listen(PORT, () => {
