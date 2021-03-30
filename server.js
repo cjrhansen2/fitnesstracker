7e116fca77616bd94d79dcf39fe004c108a41747
cjrhansen2 <cjrhansen2@gmail.com>
2021-03-30 15:07:23 -0700

    server

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(express.static("public"));
 //make the mongoose connection to connect to mongodb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
